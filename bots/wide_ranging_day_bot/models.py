1d5c99abfb80ab6aeaea46fe146837171ae0a20e
antonguzun <andyguzun@domclick.ru>
2021-05-16 17:30:17 +0300

    убрал хардкод параметров

--- bots/wide_ranging_day_bot/models.py
+++ bots/wide_ranging_day_bot/models.py
@@ -4,14 +4,15 @@ from decimal import Decimal
 from tinvest import Candle
 
 
+@dataclass
 class StrategyParams:
     """
     n1 - кол-во дней перед днем с широким диапазоном, включенных в период PTR
     k - коэффициент волатильности - значение, которое должен превысить volatily ratio,
     чтобы определить день с широким диапазоном
     """
-    n1 = 3
-    k = 1.7
+    n1: int
+    k: float
 
 
 @dataclass
